96903bc6ed8cdfaed6477d9914386b712a7cbe50
DESKTOP-H29UOQ1\bobpham <bobpham.tdp@gmail.com>
2023-10-14 22:20:04 +1100

    refactorize code

--- TdpGisApi_Solution/src/TdpGisApi.Application/DataProviders/Cosmos/Repos/CosmosRepositoryResult.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Net;
-
-namespace TdpGisApi.Application.DataProviders.Cosmos.Repos;
-
-public record CosmosRepositoryResult<T>(HttpStatusCode StatusCode, T Model, string ErrorMessage);
